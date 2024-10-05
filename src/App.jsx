5941828d720e7c8233a1d2042512cea8347cc321
brunorwanda4 <brunorwanda4@gmail.com>
2024-10-05 17:44:29 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
-// import Nav from "./components/Nav"
-import Age18 from "./assets/component/age18"
 
 const App = () => {
-  // const name = "Allen";
-  
-  // if (name === "a") {
-  //   return (
-  //     <div  className="w-full h-screen items-center justify-center flex flex-col gap-2 bg-gray-100">
-  //       <p>name is : {name}</p>
-  //     </div>
-  //   )
-  // }
 
   return(
   <div className="w-full h-screen items-center justify-center flex flex-col gap-2 bg-gray-100">
-    {/* <Nav /> */}
     <div className=" flex gap-2 items-center">
-      <Age18/>
-      {/* <button className=" btn btn-block bg-gray-300 outline-none border-none">Number + 1</button> */}
-      {/* <p className="  ">name is not allen is :{name}</p> */}
+      <form action="">
+        <input type="text" className=" input " />
+        <input type="text" />
+        <button>submit</button>
+      </form>
     </div>
   </div>
   )
